dc3c410db2c5db74f3760a19300293da9931c237
zeroappz <72295783+zeroappz@users.noreply.github.com>
2022-10-08 13:15:31 +0530

    first commit

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'freelancer';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['pre'] = 'fl_';
+
+$config['admin_folder'] = 'admin';
+$config['version'] = '1.0';
+$config['installed'] = '1';
+?>
